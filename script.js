fe91bfdb762a24ba439a2e38011e24f216eb77b5
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-14 14:45:06 -0500

    added 1st 3 current weather stats

--- script.js
+++ script.js
@@ -24,17 +24,31 @@ $.ajax({
 
         var currentWeatherDiv = $("<div>");
         var currentCityHeaderDiv = $("<h4>");
+        var tempDiv = $("<p>");
+        var humidityDiv = $("<p>");
+        var windDiv = $("<p>");
         var currentCityName = response.name;
-        var currentDate = moment().format(" M/DD/YYYY");
+        var currentDate = moment().format(" (M/DD/YYYY)");
         var currentWeatherIconImgDiv = $("<img>");
         var currentWeatherIconImgCode = response.weather[0].icon
         var currentWeatherIconImgSrc = "http://openweathermap.org/img/wn/" + currentWeatherIconImgCode + "@2x.png";
+        var currentTemp = Math.round((response.main.temp) * 10) / 10;
+        var currentHumidity = response.main.humidity;
+        var currentWindSpeed = Math.round((response.wind.speed) * 10) / 10;
 
         currentCityHeaderDiv.append(currentCityName);
         currentCityHeaderDiv.append(currentDate);
         currentWeatherIconImgDiv.attr("src", currentWeatherIconImgSrc);
         currentCityHeaderDiv.append(currentWeatherIconImgDiv);
         currentWeatherDiv.append(currentCityHeaderDiv);
+        tempDiv.text("Temperature: " + currentTemp + " °F");
+        humidityDiv.text("Humidity: " + currentHumidity + "%");
+        windDiv.text("Wind Speed: " + currentWindSpeed + " MPH");
+
+        currentWeatherDiv.append(tempDiv);
+        currentWeatherDiv.append(humidityDiv);
+        currentWeatherDiv.append(windDiv);
+
         $(".currentConditions").append(currentWeatherDiv);
 
     });
