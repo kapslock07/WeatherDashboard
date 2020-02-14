804f704c6b62373eb003dc2b8e696463f7cf48e6
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-14 13:19:29 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,17 @@
+// This is our API key
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=" + APIKey;
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+    });
